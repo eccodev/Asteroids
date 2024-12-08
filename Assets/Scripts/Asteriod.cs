eee85827251e6730440ba821c5073283d6dadd0a
Robobanks12 <alex.macgregor@rogers.com>
2024-12-07 23:10:21 -0500

    Initial Asteroid sprites and code

---
+++ Assets/Scripts/Asteriod.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Asteriod : MonoBehaviour
+{
+    public Sprite[] sprites;
+
+    public float size = 1.0f;
+
+    public float minSize = 0.5f;
+
+    public float maxSize = 1.5f;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _rigidbody = GetComponent<_Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _spriteRenderer.sprite = sprites[Random.Range(0, sprites.Lenght)];
+
+        this.transform.eulerAngles = new Vector3(0.0f, 0.0f, Random.value * 360.0f);
+        this.transform.localScale = Vector3.one * this.size;
+    }
+}
